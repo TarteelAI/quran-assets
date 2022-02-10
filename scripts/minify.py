631e7870cd08885a0e0611b7f6313dd53b187578
Mohamed Moussa <mmmoussa@users.noreply.github.com>
2022-02-09 22:41:42 -0500

    Generate audio timing manifest for Mishari Al Afasy recitation (#20)
    * Generate audio timings manifest for Mishari Al Afasy
    
    * Minify manifest file

---
+++ scripts/minify.py
@@ -0,0 +1,23 @@
+# This script pulls data from the QDC api to generate standardized data files
+# for each page of the Quran
+
+
+import io
+import json
+import os
+import requests
+
+file_path = '../audio/mishari-manifest.json'
+
+def main():
+    dirname = os.path.dirname(__file__)
+    with open(os.path.join(dirname, file_path), "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+
+    with io.open(os.path.join(dirname, file_path), 'w', encoding='utf8') as json_file:
+        formatted_data = json.dumps(data, ensure_ascii=False)
+        json_file.write(formatted_data)
+
+
+if __name__ == "__main__":
+    main()
